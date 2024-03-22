6eae0c464e2accf26f97a9b9781f26c0da2ec9d9
devilzac <shane199274@gmail.com>
2024-03-22 13:13:00 -0500

    responsive menu

--- resources/views/components/nav-mystera.blade.php
+++ resources/views/components/nav-mystera.blade.php
@@ -1,21 +1,21 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-    <a class="navbar-brand" href="{{ url('/') }}">Mystera Legacy Tracker</a>
-    <div class="" id="navbarSupportedContent">            
-            <!-- START SEARCH -->
-            <form action={{ route('character.search') }} class="d-flex" role="search" method="GET">
-                <div class="cntSearch">
-                    <div class="form-switch">
-                        <input type="search" id="search" name="search" class="form-control me-2" placeholder="Character name..." aria-label="Search">                    
-                        <div class="btnAlt">
-                            <input class="form-check-input" type="checkbox" name="searchType" role="switch" id="searchType">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+    <div class="w-100 sm:flex md:flex-row">
+        <a class="navbar-brand w-100" href="{{ url('/') }}">Mystera Legacy Tracker</a>
+        <div class="w-100" id="navbarSupportedContent">            
+                <!-- START SEARCH -->
+                <form action={{ route('character.search') }} class="flex" role="search" method="GET">
+                    <div class="cntSearch float-end w-100">
+                        <div class="form-switch  w-100">
+                            <input type="search" id="search" name="search" class="form-control me-2 sm:w-100" placeholder="Character name..." aria-label="Search">                    
+                            <div class="btnAlt">
+                                <input class="form-check-input" type="checkbox" name="searchType" role="switch" id="searchType">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </form>            
-            <!-- END SEARCH -->
-    </div>
+                </form>            
+                <!-- END SEARCH -->
+        </div>
     </div>
 </nav>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 @auth()
-<nav x-data="{ open: false }" class="dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-black z-50 relative">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -7,7 +7,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <x-application-logo class="z-50 block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
